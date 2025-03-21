19ab1ec37208e54499c6156b34e4658dc4c9bece
Hüseyin USTA <h.usta@etu.edu.tr>
2025-03-21 20:52:10 +0300

    models.py dosyasına convnext modelini başlatan fonksiyon eklendi ve readme dosyası hazırlandı, readme dosyası kodlarımızın nasıl bir araya getirip çalıştırılacağını anlatıyor.

--- Codes/models.py
+++ Codes/models.py
@@ -3,6 +3,7 @@ import timm
 from torch import nn
 from torch.nn import functional as F
 
+# EfficientNet ailesinden istenilen modeli imagenet ağırlıkları ile başlatır ve sonuna dropout+linear ekler.
 def create_efficientnet_model(model_name="efficientnet_b5"):
     model = timm.create_model(model_name, pretrained=True)
     
@@ -14,4 +15,28 @@ def create_efficientnet_model(model_name="efficientnet_b5"):
         nn.Linear(num_features, 2)
     )
 
+    return model
+
+# ConvNext Base modelini imagenet ağırlıkları ile başlatır ve sonuna özel bir classifier katmanı (head) ekler.
+def create_convnext(num_classes=2, freeze_at_beginning=True):
+    model = timm.create_model('convnext_base', pretrained=True)
+    
+    # Backbone dışındaki (head dışındaki) tüm parametreleri donduruyoruz
+    if freeze_at_beginning:
+        for name, param in model.named_parameters():
+            if 'head' not in name:
+                param.requires_grad = False
+        
+    if hasattr(model, 'global_pool'):
+        model.global_pool = nn.Identity()
+    
+    model.head = nn.Sequential(
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Linear(1024, 512),
+        nn.ReLU(),
+        nn.Dropout(0.4),
+        nn.Linear(512, num_classes)
+    )
+    
     return model
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Bil-481
 
 Bu repo Bil 481 dersinin projesi için oluşturulmuştur, toplantı tutanakları, kod, açıklamalar, gereksinimler ve proje ile ilgili dosyalar bu repoda yer alacaktır.
+
+Not: Python notebookumuzu yüklemedik fakat eğer kodlarımızı çalıştırmak isterseniz yapmanız gerekenler çok basit!
+Bu kodu çalıştırmak için yapnamız gerekenler:
+1) Bir ipynb dosyası oluşturun.
+2) models.py den istediğiniz modeli initialize edin.
+3) dataset.py den de yazdığımız datasetin iki instancesini oluşturun, burada Albumentations kütüphanesini kullanarak bir transformu parametre olarak kullanabilirsiniz. Bunlardan biri validation yapmak için, diğeri ise training yapmak için kullanılacak.
+4) Oluşturduğunuz datasetini veya datasetlerini kullanarak DataLoader (torch) oluşturun.
+5) train.py den ise train için yazdığımız metodu dataloaderlerinizle seçtiğiniz öğrenme parametreleriyle çalıştırın.

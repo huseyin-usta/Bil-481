cb07a4e2c4303c49ecdf1def355a84b530544120
Hüseyin USTA <h.usta@etu.edu.tr>
2025-03-25 12:51:49 +0300

    Mobilenet modelini başlatan fonksiyon models.py dosyasına eklendi ve resnet modelini yaratan fonksiyon resnet ailesinden parametre olarak verilen modeli başlatacak şekilde jenerikleştirildi.

--- Codes/models.py
+++ Codes/models.py
@@ -57,9 +57,9 @@ def create_chexnet(freeze=True):
     
     return model
 
-# Resnet 152 modelini imagenet ağırlıkları ile başlatır.
-def create_resnet152(freeze=True):
-    model = timm.create_model("resnet152", pretrained=True)
+# Resnet ailesinden parametre olarak verilen modeli imagenet ağırlıkları ile başlatır.
+def create_resnet_model(model_name = "resnet152", freeze=True):
+    model = timm.create_model(model_name=model_name, pretrained=True)
     
     in_features = model.fc.in_features
     model.fc = nn.Linear(in_features, 2)
@@ -86,4 +86,34 @@ def create_densenet(freeze=True):
         for param in model.classifier.parameters():
             param.requires_grad = True
     
+    return model
+
+# Mobilenet v3 small 100 modelini imagenet ağırlıkları ve özel bir classifier ile başlatır.
+def create_mobilenet_model(num_classes=2, freeze_at_start=True):
+    model = timm.create_model('mobilenetv3_small_100', pretrained='imagenet')
+
+    if freeze_at_start:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    in_features = model.classifier.in_features
+
+    model.classifier = nn.Sequential(
+        nn.Linear(in_features, 512),
+        nn.BatchNorm1d(512),
+        nn.ReLU(),
+        nn.Dropout(0.4),
+
+        nn.Linear(512, 256),
+        nn.BatchNorm1d(256),
+        nn.ReLU(),
+        nn.Dropout(0.3),
+
+        nn.Linear(256, 128),
+        nn.ReLU(),
+        nn.Dropout(0.2),
+
+        nn.Linear(128, num_classes)
+    )
+
     return model
